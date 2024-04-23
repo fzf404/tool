e65f0e06d277c7cbff69cc297af254abaec4e1f6
fzf404 <me@fzf404.art>
2024-04-23 16:10:00 +0800

    fix: script url

--- script/sylu.user.js
+++ script/sylu.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         沈阳理工大学教务系统自动评价
 // @namespace    https://github.com/fzf404/tools
-// @version      0.1.2
+// @version      0.1.3
 // @description  用于自动完成教师评价，只适配了沈阳理工大学教务系统。
 // @author       fzf404
 // @homepage     https://tools.fzf404.art/script
-// @downloadURL  https://tools.fzf404.art/script/sylu.user.js
-// @updateURL    https://tools.fzf404.art/script/sylu.user.js
 // @match        https://jxw.sylu.edu.cn/xspjgl/xspj_cxXspjIndex.html*
 // @icon         https://www.sylu.edu.cn/images/favicon.ico
 // @license      MIT
